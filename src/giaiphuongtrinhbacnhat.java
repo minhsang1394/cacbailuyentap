09cdf9fb7e473130b2b0ac5dbaab654655b961f8
minh sang <minhsang1394@gmail.com>
2018-07-29 23:10:27 +0700

    codesua

---
+++ src/giaiphuongtrinhbacnhat.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class giaiphuongtrinhbacnhat {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap vao so a:");
+        a = sc.nextFloat();
+        System.out.print("nhap vao so b:");
+        b = sc.nextFloat();
+
+        if (a ==0) {
+            if (b != 0) {
+                System.out.print("phuong trinh vo nghiem ");
+            }
+            else {
+                    System.out.print("phuong trinh vo so nghiem");
+                }
+            }
+
+         else{
+            double x = (double) -b / a;
+            System.out.print("nghiem phuong trinh la : "+x );
+                }
+
+
+    }
+}
