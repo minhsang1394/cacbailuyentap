5c1844b5477d5456d507a0e9a85f83e4bbc2163b
minh sang <minhsang1394@gmail.com>
2018-08-01 00:42:34 +0700

    giai_phuong_trinh_bac_2 code chạy chuẩn , in ra chưa đẹp

--- src/giai_phuong_trinh_bac_hai.java
+++ src/giai_phuong_trinh_bac_hai.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+import static java.lang.Math.floor;
 import static java.lang.Math.sqrt;
 
 public class giai_phuong_trinh_bac_hai {
@@ -7,7 +8,7 @@ public class giai_phuong_trinh_bac_hai {
         double a;
         double b;
         double c;
-        double x;
+        System.out.print("phuong trinh tong quat co dang aX*X+bX+c=0");
         Scanner sc = new Scanner(System.in);
         System.out.printf("nhap vao gia tri cua a");
         a = sc.nextFloat();
@@ -19,15 +20,15 @@ public class giai_phuong_trinh_bac_hai {
             if (b == 0) {
                 System.out.printf("phuong trinh vo nghiem");
             } else {
-                double nghiem = -b / a;
-                System.out.printf("nghiem cua phuong trinh la:" +nghiem);
+                double nghiem = b / c;
+                System.out.printf("nghiem cua phuong trinh la: " +nghiem);
             }
 
         }else {
             double delta =  (b)*(b)-4*a*c;
             if (delta>0) {
-                double nghiem1 = (-b + sqrt(delta))/(2*a);
-                double nghiem2 = (-b - sqrt(delta))/(2*a);
+                double nghiem1 = (-b + Math.sqrt(delta))/(2*a);
+                double nghiem2 = (-b - Math.sqrt(delta))/(2*a);
                 System.out.printf("phuong trinh co 2 nghiem phan biet la:" +nghiem1);
                 System.out.printf(":" +nghiem2);
                 }
